ea86c0320d983c931ceb184008200e9d2e9a8620
jyu7771992 <jyu7771992@gmail.com>
2022-02-15 23:08:52 -0800

    built the structure of LoadNibNamed

---
+++ TableController_LoadNibNamed/TableController_LoadNibNamed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableController_LoadNibNamed
+//
+//  Created by Peipei Yu on 2/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
